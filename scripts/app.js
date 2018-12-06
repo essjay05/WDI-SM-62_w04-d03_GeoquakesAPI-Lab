11fbdbb562af13ab7fa0070bce283f480dad8f9c
Joy <stepjoyous@gmail.com>
2018-12-05 17:17:40 -0800

    Added markers to map

--- scripts/app.js
+++ scripts/app.js
@@ -6,29 +6,42 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 axios.get(weekly_quakes_endpoint) 
     .then(res => {
       let earthLoc = document.getElementById('info');
-      
+    // This is for the quake info  
       let quakes =res.data.features;
       quakes.forEach( i => {
         let title = document.createElement('p');
         qInfo = i.properties.title;
         earthLoc.appendChild(title)
         title.innerHTML = qInfo
-      });
+    // This is for the coordinates
+        var coords = i.geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+        });
+      })
     });
 
-//     console.log(res);
-//     console.log("Let's get coding!"); 
-//   });
-  // CODE IN HERE!
+// GET COORDINATES to input to Google Map:
+// axios.get(weekly_quakes_endpoint)
+//     .then(res=> {
+//       let quakes = res.data.features;
+//       quakes.forEach( i=> {
+//         coord = i.geometry.coordinates;
+//       console.log(coord);
+//     })
+
+
+
 
 // Creating MAP using coordinates
 
-var map;
-// document.ready(function() {
+var map
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 34.013, lng: -118.495},
           zoom: 8
-        });
+        })
       }
-initMap(); 
+initMap()
