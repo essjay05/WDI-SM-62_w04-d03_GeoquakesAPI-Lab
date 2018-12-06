8c1e22956420163f399f4152ef277eb63285ea73
Joy <stepjoyous@gmail.com>
2018-12-05 16:47:28 -0800

    Added map to index.html

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ console.log('Loaded');
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-
+// ADD LOCATIONS TO INDEX.HTML
 axios.get(weekly_quakes_endpoint) 
     .then(res => {
       let earthLoc = document.getElementById('info');
@@ -15,18 +15,20 @@ axios.get(weekly_quakes_endpoint)
         title.innerHTML = qInfo
       });
     });
-      // let quake = res.data.features;
-   
-  // //     let earthLoc = document.getElementById('info');
-  
-  //     res.data.data.features.forEach( i => {
-  //         let title = document.createElement('p');
-  //         title.src = i.properties.title;
-  //         imageEl.appendChild(image);
-  //     });
-  // });
 
 //     console.log(res);
 //     console.log("Let's get coding!"); 
 //   });
   // CODE IN HERE!
+
+// Creating MAP using coordinates
+
+var map;
+// document.ready(function() {
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 34.013, lng: -118.495},
+          zoom: 8
+        });
+      }
+initMap(); 
